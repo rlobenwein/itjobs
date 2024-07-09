7a96ed7910761d21d033809dd4f246903f62c406
Rodrigo Lobenwein <rodrigolobenweindev@gmail.com>
2024-07-09 16:19:25 -0300

    Add user controller

---
+++ src/controllers/usersController.js
@@ -0,0 +1,33 @@
+import User from '../models/user.js';
+
+export const createUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const user = await User.create({ name, email, password });
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+export const getUsers = async (req, res) => {
+  try {
+    const users = await User.findAll();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+export const getUserById = async (req, res) => {
+  try {
+    const user = await User.findByPk(req.params.id);
+    if (user) {
+      res.status(200).json(user);
+    } else {
+      res.status(404).json({ message: 'User not found' });
+    }
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
